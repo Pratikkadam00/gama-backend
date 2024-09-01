a9c3009fb7eced9b4a8340ec1a77d90c04072e46
pratikkadam00 <pratikadam00@gmail.com>
2024-09-01 22:14:12 +0530

    updated

--- app.js
+++ app.js
@@ -6,11 +6,15 @@ import authRoutes from "./routes/authRoutes.js";
 import cors from "cors";
 
 dotenv.config();
-
 const app = express();
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const MONGO_URI = process.env.MONGO_URI;
+
 app.use(cors());
+
 app.get("/", (req, res) => {
   res.send("Healthy");
 });

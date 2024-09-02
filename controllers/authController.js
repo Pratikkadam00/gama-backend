4c332b2724b6ce284e538487d7b365eafcb0c005
pratikkadam00 <pratikadam00@gmail.com>
2024-09-02 17:03:37 +0530

    updated

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,6 +6,11 @@ import { generateToken } from "../utils/jwtUtils.js";
 export const register = async (req, res) => {
   const { name, email, password } = req.body;
   try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ error: "User already exists" });
+    }
+
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = new User({
       name,
@@ -13,12 +18,13 @@ export const register = async (req, res) => {
       password: hashedPassword,
     });
     await user.save();
-    res.status(200).json({
+
+    res.status(201).json({
       message: "User registered successfully",
       user: { name, email, userId: user.id },
     });
   } catch (error) {
-    console.log(error);
+    console.error(error);
     res.status(500).json({ error: "Internal server error" });
   }
 };
@@ -28,8 +34,11 @@ export const login = async (req, res) => {
 
   try {
     const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
 
-    if (!user || !(await bcrypt.compare(password, user.password))) {
+    if (!(await bcrypt.compare(password, user.password))) {
       return res.status(400).json({ error: "Invalid credentials" });
     }
 
@@ -41,6 +50,7 @@ export const login = async (req, res) => {
       user: { name: user.name, email: user.email, userId: user._id },
     });
   } catch (error) {
+    console.error(error);
     res.status(500).json({ error: "Internal server error" });
   }
 };

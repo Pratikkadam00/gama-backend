fe605df64c56cfc7c102fd15f2049695b0a360f1
pratikkadam00 <pratikadam00@gmail.com>
2024-09-02 16:57:33 +0530

    updated

--- app.js
+++ app.js
@@ -9,7 +9,11 @@ import helmet from "helmet";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: 'https://gama-frontend-chi.vercel.app', // Specify the allowed origin
+  credentials: true // If you need to allow credentials
+}));
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
